98a7bb41c7c4ef0d2958341b62a68c6e2481845c
TrollSkull <leanahuelmiranda@gmail.com>
2023-07-19 14:55:51 -0300

    Add files via upload

--- src/main/java/me/trollskull/tntreporter/Events.java
+++ src/main/java/me/trollskull/tntreporter/Events.java
@@ -3,14 +3,17 @@ package me.trollskull.tntreporter;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockBreakEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
-import java.text.MessageFormat;
 import org.bukkit.block.Block;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Location;
 import org.bukkit.Bukkit;
 
 public class Events implements Listener {
@@ -27,8 +30,7 @@ public class Events implements Listener {
  
         int currentCount = Main.tntBrokenByPlayers.getOrDefault(playerName, 0);
         Main.tntBrokenByPlayers.put(playerName, currentCount + 1);
-
-        logCoordinates(block, "has broken", player);
+        // logCoordinates(block, "has broken", player);
     }
 
     @EventHandler
@@ -45,6 +47,10 @@ public class Events implements Listener {
         if (action == null || action != Action.RIGHT_CLICK_BLOCK || item == null || item.getType() != Material.FLINT_AND_STEEL)
             return;
 
+        String playerName = player.getName();
+        int currentCount = Main.tntPlacedByPlayers.getOrDefault(playerName, 0);
+        Main.tntPlacedByPlayers.put(playerName, currentCount + 1);
+
         logCoordinates(block, "activated", player);
     }
 
@@ -53,10 +59,68 @@ public class Events implements Listener {
         Block block = event.getBlock();
         Player player = event.getPlayer();
 
-        if (block == null || block.getType() != Material.TNT)
-            return;
+        if (block.getType() == Material.TNT) {
+            int x = block.getLocation().getBlockX();
+            int y = block.getLocation().getBlockY();
+            int z = block.getLocation().getBlockZ();
 
-        logCoordinates(block, "has placed", player);
+            String playerName = player.getName();
+            int currentCount = Main.tntPlacedByPlayers.getOrDefault(playerName, 0);
+            Main.tntPlacedByPlayers.put(playerName, currentCount + 1);
+
+            TextComponent consoleMessage = new TextComponent(ChatColor.RED + Main.PREFIX + ChatColor.WHITE + " Player ");
+            consoleMessage.addExtra(ChatColor.WHITE + player.getName() + " has placed TNT on ");
+            TextComponent coordsComponent = new TextComponent(ChatColor.GREEN + "[" + x + ", " + y + ", " + z + "]");
+            consoleMessage.addExtra(coordsComponent);
+            consoleMessage.addExtra(ChatColor.WHITE + " !");
+
+            String legacyText = consoleMessage.toLegacyText();
+            // player.sendMessage(legacyText);
+            Bukkit.getServer().getConsoleSender().sendMessage(legacyText);
+            // logCoordinates(block, "has placed", player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        Player player = event.getPlayer();
+
+        if (action == Action.RIGHT_CLICK_BLOCK || action == Action.RIGHT_CLICK_AIR) {
+            if (player.hasPermission("ServerOperator")) {
+                String message = event.getItem() != null ? event.getItem().getItemMeta().getDisplayName() : null;
+                
+                if (message != null && message.startsWith(Main.PREFIX)) {
+                    String[] parts = message.split(":");
+                    
+                    if (parts.length == 2) {
+                        String[] coords = parts[1].split(",");
+                        
+                        if (coords.length == 3) {
+                            try {
+                                int x = Integer.parseInt(coords[0].trim());
+                                int y = Integer.parseInt(coords[1].trim());
+                                int z = Integer.parseInt(coords[2].trim());
+
+                                Location tpLocation = new Location(player.getWorld(), x + 0.5, y, z + 0.5);
+                                player.teleport(tpLocation);
+
+                                TextComponent clickableMessage = new TextComponent(ChatColor.RED + Main.PREFIX + ChatColor.WHITE + " Player ");
+                                clickableMessage.addExtra(ChatColor.WHITE + player.getName() + " activated TNT on ");
+                                TextComponent coordsComponent = new TextComponent(ChatColor.GREEN + "[" + x + ", " + y + ", " + z + "]");
+                                coordsComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp @s " + x + " " + y + " " + z));
+                                clickableMessage.addExtra(coordsComponent);
+                                clickableMessage.addExtra(ChatColor.WHITE + " !");
+
+                                player.spigot().sendMessage(clickableMessage);
+                            } catch (NumberFormatException ignored) {
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private void logCoordinates(Block block, String action, Player player) {
@@ -64,11 +128,17 @@ public class Events implements Listener {
         int y = block.getLocation().getBlockY();
         int z = block.getLocation().getBlockZ();
 
-        String pattern = "{0}Player '{1}' {2} TNT on [{3}, {4}, {5}] !";
-        String message = MessageFormat.format(
-            pattern, Main.prefix, player.getName(), action, x, y, z);
+        String playerName = player.getName();
+        String coordinates = ChatColor.GREEN + "[" + x + ", " + y + ", " + z + "]";
 
-        Bukkit.getServer().getConsoleSender().sendMessage(message);
+        TextComponent message = new TextComponent(ChatColor.RED + Main.PREFIX + ChatColor.WHITE + " Player ");
+        message.addExtra(ChatColor.WHITE + playerName + " " + action + " TNT on ");
+        TextComponent coordsComponent = new TextComponent(coordinates);
+        coordsComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp @s " + x + " " + y + " " + z));
+        message.addExtra(coordsComponent);
+        message.addExtra(ChatColor.RED + " !");
+
+        Bukkit.getServer().getConsoleSender().sendMessage(message.toLegacyText());
         Main.sendWarnToAdmin(message);
     }
 }
--- src/main/java/me/trollskull/tntreporter/Main.java
+++ src/main/java/me/trollskull/tntreporter/Main.java
@@ -1,19 +1,29 @@
 package me.trollskull.tntreporter;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import java.util.HashMap;
+import org.bukkit.Bukkit;
 import java.util.Map;
 
 public class Main extends JavaPlugin {
     public static Map<String, Integer> tntBrokenByPlayers = new HashMap<>();
-    public static Object prefix;
+    public static Map<String, Integer> tntPlacedByPlayers = new HashMap<>();
+    public static String PREFIX = ChatColor.RED + "[TNT Reporter] " + ChatColor.RESET;
+    public FileConfiguration config;
 
     @Override
     public void onEnable() {
-        getLogger().info("TNT Reporter is enabled! [v0.0.5]");
+        getLogger().info("TNT Reporter is enabled! [v0.0.24]");
         getServer().getPluginManager().registerEvents(new Events(), this);
+
+        saveDefaultConfig();
+        config = getConfig();
     }
 
     @Override
@@ -21,10 +31,39 @@ public class Main extends JavaPlugin {
         getLogger().info("TNT Reporter is disabled.");
     }
 
-    public static void sendWarnToAdmin(String message) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("tntreport")) {
+            sender.sendMessage("TNT Report:");
+            sender.sendMessage("TNT broken by players:");
+            for (Map.Entry<String, Integer> entry : tntBrokenByPlayers.entrySet()) {
+                String playerName = entry.getKey();
+                int tntBroken = entry.getValue();
+                sender.sendMessage(playerName + ": " + tntBroken);
+            }
+            
+            sender.sendMessage("TNT placed by players:");
+            for (Map.Entry<String, Integer> entry : tntPlacedByPlayers.entrySet()) {
+                String playerName = entry.getKey();
+                int tntPlaced = entry.getValue();
+                sender.sendMessage(playerName + ": " + tntPlaced);
+            }
+
+            return true;
+
+        } else if (command.getName().equalsIgnoreCase("tntreload") && sender.hasPermission("tntreporter.reload")) {
+            reloadConfig();
+            config = getConfig();
+            sender.sendMessage("TNT Reporter configuration reloaded.");
+            return true;
+        }
+        return false;
+    }
+
+    public static void sendWarnToAdmin(BaseComponent... components) {
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (player.hasPermission("ServerOperator")) {
-                player.sendMessage(message);
+                player.spigot().sendMessage(components);
             }
         }
     }
